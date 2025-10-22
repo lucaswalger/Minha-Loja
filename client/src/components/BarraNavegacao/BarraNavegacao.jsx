ba3e9ebad88f1b7e014b6fe96a78445ec630ddca
LUCAS MONTEIRO WALGER <l.walger@labcetec.net>
2025-10-21 21:42:56 -0300

    att

--- client/src/components/BarraNavegacao/BarraNavegacao.jsx
+++ client/src/components/BarraNavegacao/BarraNavegacao.jsx
@@ -1,8 +1,8 @@
-// Importando o css da Barra de navegação
-import style from "./BarraNavegacao.module.css";
+// Importando o css da barra de navegação
+import styles from "./BarraNavegacao.module.css";
 
-// Importação dos componentes do bootstrap
-import {Nav, Navbar, NavDropdown, Image, Accordion} from "react-bootstrap";
+// Importar os componentes do bootstrap
+import { Nav, Navbar, NavDropdown, Image, Accordion } from "react-bootstrap";
 
 // Importando os links do router
 import { NavLink } from "react-router-dom";
@@ -11,28 +11,79 @@ import { NavLink } from "react-router-dom";
 import { AuthContext } from "../../contexts/UserContext.jsx";
 import { useContext } from "react";
 
-// Importação dos ícones
-import {BsBoxes} from "react-icons/bs"
+// Importanto os icones
+import { BsBoxes } from "react-icons/bs";
 
 const BarraNavegacao = () => {
+  // importar o nome de usuario logado e funcao logout
+  const { usuarioNome, logout } = useContext(AuthContext);
 
-    // Importar o nome do usuário logado e funcão de logout
-    const { usuarioNome, logout } = useContext(AuthContext);
-    
-    // Guarda o id do usuário logado
-    const idAtual = localStorage.getItem("usuarioId");
+  // Guarda o id do usuário atual
+  const idAtual = localStorage.getItem("id");
 
-    // Guarda a imagem do usuário logado
-    const imagemAtual = localStorage.getItem("imagemPerfil");
+  // Guarda a imagem do usuário atual
+  const imagemAtual = localStorage.getItem("imagemPerfil");
 
-    // Imagem padrão caso o usuário não tenha uma
-    const imagemPadrao = "https://www.pngall.com/wp-content/uploads/5/Profile-PNG-High-Quality-Image.png";
+  // Imagem padrão
+  const semImagem = "https://www.pngall.com/wp-content/uploads/5/Profile-PNG-High-Quality-Image.png";
+  return (
+    <div
+      className="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark min-vh-100 max-vh-100"
+      style={{ width: "250px" }}
+    >
+      {/* Logo da empresa */}
+      <Navbar.Brand as={NavLink} to="/home" className="text-white mb-3">
+        <BsBoxes className="fs-4" />
+        <span className="fs-5 ms-2">C&G Solutions</span>
+      </Navbar.Brand>
 
-  return(
-    <div classsName = "d-flex flex-column flex-shrink-0 p-3 text-white bg-dark">
-       Alo
+      {/* Opções de menu */}
+      <Nav className="flex-column mb-auto">
+       
+        {/* Opção home */}
+        <Nav.Link as={NavLink} to="/home" className="text-white px-2">
+          <BsBoxes className="fs-4" />
+          <span className="fs-5 ms-2">Home</span>
+        </Nav.Link>
+
+        {/* Opção Relatórios */}
+        <Nav.Link as={NavLink} to="/relatorios" className="text-white px-2">
+          <BsBoxes className="fs-4" />
+          <span className="fs-5 ms-2">Relatórios</span>
+        </Nav.Link>
+
+        {/* Criando o arcordeon */}
+        <Accordion flush className="flex-column mb-auto">
+
+          {/* Páginas produtos */}
+          <Accordion.Item className="bg-dark text-white">  
+              <Accordion.Header className={styles.accordionHeader}>
+                <BsBoxes className="fs-4"/>
+                <span className="ms-2"> Produtos </span>
+              </Accordion.Header>
+              <Accordion.Body>
+                <Nav className="flex-column">
+                    {/* Opção 1 */}
+                    <Nav.Link as={NavLink} to="/produtos"
+                              className="text-white ps-4">
+                      <BsBoxes className="fs-5"/>
+                      <span className="ms-2"> Listar </span>        
+                    </Nav.Link>
+                    {/* Opção 2 */}
+                    <Nav.Link as={NavLink} to="/produtos/cadastrar"
+                              className="text-white ps-4">
+                      <BsBoxes className="fs-5"/>
+                      <span className="ms-2"> Adicionar </span>
+                    </Nav.Link>
+                </Nav>
+              </Accordion.Body>
+          </Accordion.Item>
+          {/* fim produtos */}
+        
+        </Accordion>
+      </Nav>
     </div>
-  )
-}
+  );
+};
 
 export default BarraNavegacao;
